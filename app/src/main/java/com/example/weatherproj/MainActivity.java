0b3644527d7d906dcb3514a2d3b8c64d69f4d91e
Pallavi <96259165+pallavipan99@users.noreply.github.com>
2022-02-08 14:00:00 -0500

    Add full weather fetching functionality with hourly display

--- app/src/main/java/com/example/weatherproj/MainActivity.java
+++ app/src/main/java/com/example/weatherproj/MainActivity.java
@@ -71,5 +71,124 @@ public class MainActivity extends AppCompatActivity {
     ImageView imag3;
     ImageView imag4;
     TextView date;
+    // Display current date and time
+    Date thisDate = new Date();
+    SimpleDateFormat dateForm = new SimpleDateFormat("MM/dd/y hh:mm a");
+    date.setText(dateForm.format(thisDate));
 
+    zipCode.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            zipC = charSequence.toString();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    });
+
+    run.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AsyncTaskDownloadClassThing asyncTask = new AsyncTaskDownloadClassThing();
+            asyncTask.execute();
+
+            if (!(latitude.equals("") && longitude.equals(""))) {
+                AsyncTaskDownloadClassThing2 asyncTask2 = new AsyncTaskDownloadClassThing2();
+                asyncTask2.execute();
+            }
+        }
+    });
+}
+
+// AsyncTask classes with weather fetching logic
+public class AsyncTaskDownloadClassThing extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        key = String.valueOf(zipCode.getEditableText());
+
+        try {
+            URL weatherKey = new URL("https://api.openweathermap.org/geo/1.0/zip?zip=" + zipC + ",us&appid=dfb78e5904fad1458c4311b74909d54f");
+            URLConnection keyConn = weatherKey.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(keyConn.getInputStream()));
+            String completeString = "", data = reader.readLine();
+
+            while (data != null) {
+                completeString += data;
+                data = reader.readLine();
+            }
+            firstString = completeString;
+            JSONObject url = new JSONObject(completeString);
+            latitude = "Latitude: " + url.getString("lat");
+            lati = url.getString("lat");
+            longitude = "Longitude: " + url.getString("lon");
+            longi = url.getString("lon");
+            town = "City: " + url.getString("name");
+
+            runOnUiThread(() -> {
+                lat.setText(latitude);
+                longit.setText(longitude);
+                city.setText(town);
+            });
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+public class AsyncTaskDownloadClassThing2 extends AsyncTask<Void, Void, Void> {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL secondWeatherKey = new URL("https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=daily,minutely,alerts,current&units=imperial&appid=dfb78e5904fad1458c4311b74909d54f");
+            URLConnection fin = secondWeatherKey.openConnection();
+            BufferedReader read = new BufferedReader(new InputStreamReader(fin.getInputStream()));
+            String weather = "", data2 = read.readLine();
+
+            while (data2 != null) {
+                weather += data2;
+                data2 = read.readLine();
+            }
+            secString = weather;
+            JSONObject currentWeather = new JSONObject(secString);
+            JSONArray array = new JSONArray(currentWeather.getString("hourly"));
+
+            for (int i = 0; i < array.length(); i++) {
+                SimpleDateFormat date = new SimpleDateFormat("hh:mm a");
+                java.util.Date time = new java.util.Date((long) Integer.parseInt(array.getJSONObject(i).getString("dt")) * 10);
+                String ti = date.format(time);
+                arr.put(ti);
+                arr.put(array.getJSONObject(i).getString("temp"));
+                JSONArray weatherW = new JSONArray(array.getJSONObject(i).getString("weather"));
+                arr.put(weatherW.getJSONObject(0).getString("description"));
+            }
+
+            // Update TextViews and Images
+            runOnUiThread(() -> {
+                weath.setText("Time: " + arr.get(0) + " Temp: " + arr.get(1) + "° Description: " + arr.get(2));
+                weath2.setText("Time: " + arr.get(3) + " Temp: " + arr.get(4) + "° Description: " + arr.get(5));
+                weath3.setText("Time: " + arr.get(6) + " Temp: " + arr.get(7) + "° Description: " + arr.get(8));
+                weath4.setText("Time: " + arr.get(9) + " Temp: " + arr.get(10) + "° Description: " + arr.get(11));
+
+                // Images
+                if (arr.get(2).toString().contains("snow")) imag.setImageResource(R.drawable.snow);
+                if (arr.get(5).toString().contains("snow")) imag2.setImageResource(R.drawable.snow);
+                if (arr.get(8).toString().contains("snow")) imag3.setImageResource(R.drawable.snow);
+                if (arr.get(11).toString().contains("snow")) imag4.setImageResource(R.drawable.snow);
+
+                if (arr.get(2).toString().contains("clear")) imag.setImageResource(R.drawable.sky);
+                if (arr.get(5).toString().contains("clear")) imag2.setImageResource(R.drawable.sky);
+                if (arr.get(8).toString().contains("clear")) imag3.setImageResource(R.drawable.sky);
+                if (arr.get(11).toString().contains("clear")) imag4.setImageResource(R.drawable.sky);
+            });
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
